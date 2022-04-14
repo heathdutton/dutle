c47a17c8fd4563bc1614c84bceb3aadfc62c51d3
Logan Butler <LoganCButler@users.noreply.github.com>
2022-04-14 10:58:04 -0400

    Update wording

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -95,7 +95,7 @@ export const StatsModal = ({
               {SHARE_TEXT}
             </button>
           </div>
-          <div className="flex justify-center mt-5 sm:mt-6 columns-2 dark:text-white">
+          {/* <div className="flex justify-center mt-5 sm:mt-6 columns-2 dark:text-white">
             <button
               type="button"
               className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
@@ -106,7 +106,7 @@ export const StatsModal = ({
             >
               {START_NEW_GAME}
             </button>
-          </div>
+          </div> */}
         </>
       )}
     </BaseModal>
